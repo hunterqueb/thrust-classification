b900d34f6aa7bc64b19c55a7b2cda45687ae8a7d
Hunter Quebedeaux <hunter.quebedeaux@ucf.edu>
2026-03-03 15:41:51 -0500

    updated class gen script

--- runClassGen.sh
+++ runClassGen.sh
@@ -2,11 +2,13 @@
 
 # -- latest!
 
+# remove all classification results to avoid confusion 
+./removeClassResults.sh
 # run generateThrustClass.sh to generate the classification results without energy features
 ./generateThrustClass.sh
 # generate latex tables for results without energy features
 ./generateLatexTablesColor.sh
-# remove classification logs to avoid issue with energy features logs
+# remove classification logs to avoid issue with energy features logs but preserve latex table files
 ./removeClassLogs.sh
 # run generateThrustClass_energy.sh to generate the classification results with energy features
 ./generateThrustClass_energy.sh
