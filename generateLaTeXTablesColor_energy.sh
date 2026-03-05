4cfee8803073799e3a40de95563dbb210adcac18
hunterqueb <quebedeauxh55@gmail.com>
2026-03-04 20:49:02 -0500

    fixed bad arg call in table scripts

--- generateLaTeXTablesColor_energy.sh
+++ generateLaTeXTablesColor_energy.sh
@@ -6,33 +6,33 @@
 
 # # results only for vleo
   python gmat/data/classification/generateLatexTableCompact.py \
-      --csv parsed_data/vleo/_group/csv/summary_group.csv --train-label VLEO \
+      --csv gmat/data/classification/parsed_data/vleo/_group/csv/summary_group.csv --train-label VLEO \
       --out-prefix class_vleo_energy --metrics rpf1 --combine-features
 
 # results only for leo
   python gmat/data/classification/generateLatexTableCompact.py \
-      --csv parsed_data/leo/_group/csv/summary_in_group.csv --train-label LEO \
+      --csv gmat/data/classification/parsed_data/leo/_group/csv/summary_in_group.csv --train-label LEO \
       --out-prefix class_leo_energy --metrics rpf1 --combine-features
 
 # # results for geo
   python gmat/data/classification/generateLatexTableCompact.py \
-      --csv parsed_data/geo/_group/csv/summary_group.csv --train-label GEO \
+      --csv gmat/data/classification/parsed_data/geo/_group/csv/summary_group.csv --train-label GEO \
       --out-prefix class_geo_energy --metrics rpf1 --combine-features
 
 # # vleo train test, leo train with vleo test
 #   python gmat/data/classification/generateLatexTableCompact.py \
-#       --csv parsed_data/vleo/_group/csv/summary_group.csv --train-label VLEO \
-#       --csv parsed_data/leo/_group/csv/summary_group.csv  --train-label LEO \
+#       --csv gmat/data/classification/parsed_data/vleo/_group/csv/summary_group.csv --train-label VLEO \
+#       --csv gmat/data/classification/parsed_data/leo/_group/csv/summary_group.csv  --train-label LEO \
 #       --out-prefix class_vleo_leo_energy --metrics rpf1 --oe-only
 
 # # vleo train test, geo train with vleo test
 #   python gmat/data/classification/generateLatexTableCompact.py \
-#       --csv parsed_data/vleo/_group/csv/summary_group.csv --train-label VLEO \
-#       --csv parsed_data/combined/leo-meo-geo/_group/csv/summary_group.csv  --train-label LEO-MEO-GEO \
+#       --csv gmat/data/classification/parsed_data/vleo/_group/csv/summary_group.csv --train-label VLEO \
+#       --csv gmat/data/classification/parsed_data/combined/leo-meo-geo/_group/csv/summary_group.csv  --train-label LEO-MEO-GEO \
 #       --out-prefix class_vleo_combined_energy --metrics rpf1 --oe-only
 
 # combine previous two tables into one table to compactly show all results for vleo test set
   python gmat/data/classification/generateLatexTableCompact.py \
-      --csv parsed_data/leo/_group/csv/summary_group.csv --train-label LEO \
-      --csv parsed_data/combined/leo-meo-geo/_group/csv/summary_group.csv  --train-label LEO-MEO-GEO \
+      --csv gmat/data/classification/parsed_data/leo/_group/csv/summary_group.csv --train-label LEO \
+      --csv gmat/data/classification/parsed_data/combined/leo-meo-geo/_group/csv/summary_group.csv  --train-label LEO-MEO-GEO \
       --out-prefix class_leo_combined_energy --metrics rpf1 --oe-only
